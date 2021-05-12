7a46e166ab8c0a8fc9f17dc0fd243c575d75e3d5
Artec <84013701+Arthur-Njenga@users.noreply.github.com>
2021-05-12 15:44:02 +0300

    Adding a random function.

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 // An array of random greetings.
-const greetings = ["Asalaam alaikum", "Hola", "Bonjour", "Nǐn hǎo", "Salve", "Shikamoo"]
+/*const greetings = {
+    language:["Arabic", "Spanish", "French", "Chinese", "Italian", "Swahili", "Greek", "Indonesian", "Hindi", "Turkish", "Hebrew"],
+    greeting:["Asalaam alaikum", "Hola", "Bonjour", "Nǐn hǎo", "Salve", "Shikamoo", "Yassas", "Selamat siang", "Namaste", "Merhaba", "Shalom"]
+}
+*/
 
-console.log(greetings);
+const greetings =["Asalaam alaikum", "Hola", "Bonjour", "Nǐn hǎo", "Salve", "Shikamoo", "Yassas", "Selamat siang", "Namaste", "Merhaba", "Shalom"]
+
+//console.log(greetings);
+
+/*switch (greetings) {
+    case 0:
+        
+}*/
 
 // Returns a random joke.
+const randGreeting = (array) => {
+    console.log(array[Math.floor(Math.random() *greetings.length)]);
+};
+
+randGreeting(greetings);
